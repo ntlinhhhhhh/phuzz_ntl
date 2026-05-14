a1eef6c263e445e4371958559e6a78c76bea56e3
duongg <160174186+ntlinhhhhhh@users.noreply.github.com>
2026-05-14 23:38:41 +0700

    Delete experiments directory

--- experiments/02-0day-vulns/01-analyze-all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir analysis configs || true
-find plugins/ -type f -iname '*.zip' | while read fpath; do echo "Checking $fpath"; python3 ./01-analyze-plugin.py -p "$fpath"; done | tee plugin-analysis.log
--- experiments/02-0day-vulns/02-fuzz-all-plugins.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3.11 02-fuzz-all-plugin-configs.py
